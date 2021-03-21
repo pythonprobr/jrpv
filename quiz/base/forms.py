10f89800077e661d42bb41970f1e54c497db2159
Renzo <renzon@gmail.com>
2021-03-21 10:43:32 -0300

    Código inicial do projeto

--- quiz/base/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-
-from quiz.base.models import Aluno
-
-
-class AlunoForm(ModelForm):
-    class Meta:
-        model = Aluno
-        fields = ['nome', 'email']
--- quiz/base/static/base/assets/js/script.js
+++
@@ -1,6 +0,0 @@
-function showPage() {
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("game").style.display = "block";
-}
-
-window.onload = setTimeout(showPage, 1000)
